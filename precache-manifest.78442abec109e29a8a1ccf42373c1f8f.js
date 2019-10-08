25d55b9d7420a4acf2164659beefee533be187be
Höytö Teemu <s1900549@edu.bc.fi>
2019-10-08 15:34:07 +0300

    up

---
+++ precache-manifest.78442abec109e29a8a1ccf42373c1f8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f2964335a3c3e4c06122d5f447dfbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5fff3b25ea47b876f11",
+    "url": "/static/css/main.4aa5047c.chunk.css"
+  },
+  {
+    "revision": "012d70ca47e40dd38276",
+    "url": "/static/js/2.4167bea9.chunk.js"
+  },
+  {
+    "revision": "e5fff3b25ea47b876f11",
+    "url": "/static/js/main.ac0729cb.chunk.js"
+  },
+  {
+    "revision": "a92780ab0402f83397d3",
+    "url": "/static/js/runtime~main.6c879ade.js"
+  }
+]);
